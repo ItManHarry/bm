589219d8a73997363a0a9d059aaf0656ff29ff62
itmanharry <jack_chengqian@163.com>
2022-04-27 22:31:03 +0800

    Initial commit

---
+++ com/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from com.config import configurations
+from com.regs.web_globals import reg_web_global_path, reg_web_global_context
+from com.regs.web_plugins import reg_web_plugins
+from com.regs.web_shells import reg_web_shell, reg_web_commands
+def create_app(config=None):
+    if config is None:
+        config = 'pro_config'
+    app = Flask('com')
+    app.config.from_object(configurations[config])
+    reg_web_global_path(app)
+    reg_web_global_context(app)
+    reg_web_plugins(app)
+    reg_web_shell(app)
+    reg_web_commands(app)
+    return app
