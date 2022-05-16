1642a2e1a7f0cecb2f432786cb9787cb51e9142a
itmanharry <jack_chengqian@163.com>
2022-05-16 11:14:49 +0800

    add no authed template and changed login page

--- com/forms/auth.py
+++ com/forms/auth.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
-    user_id = StringField('User ID', validators=[DataRequired('Please Input User ID!'), Length(1, 20, '长度要介于(1~20)!')])
-    user_pwd = PasswordField('User Password', validators=[DataRequired('Please input password!'), Length(8, 128, '长度要介于(8~128)!')])
-    submit = SubmitField('Login')
+    user_id = StringField('账号', validators=[DataRequired('Please Input User ID!'), Length(1, 20, '长度要介于(1~20)!')])
+    user_pwd = PasswordField('密码', validators=[DataRequired('Please input password!'), Length(8, 128, '长度要介于(8~128)!')])
+    submit = SubmitField('登录')
